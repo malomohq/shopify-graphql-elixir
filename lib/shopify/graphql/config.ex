ea485263eac93ba6c5582f5f734fc485005113f8
Anthony Smith <420061+anthonator@users.noreply.github.com>
2019-06-25 12:21:38 -0400

    Added struct for holding config

---
+++ lib/shopify/graphql/config.ex
@@ -0,0 +1,45 @@
+defmodule Shopify.GraphQL.Config do
+  @type t ::
+          %__MODULE__{
+            access_token: String.t(),
+            endpoint: String.t(),
+            host: String.t(),
+            http_client: module,
+            http_client_opts: any,
+            json_codec: module,
+            path: String.t(),
+            port: String.t(),
+            protocol: String.t(),
+            shop: String.t(),
+            version: String.t()
+          }
+
+  defstruct access_token: nil,
+            endpoint: "/graphql.json",
+            host: "myshopify.com",
+            http_client: nil,
+            http_client_opts: [],
+            json_codec: nil,
+            path: "/admin/api",
+            port: nil,
+            protocol: "https",
+            shop: nil,
+            version: "2019-04"
+
+  @doc """
+  Build a config struct.
+
+  Config is composed of values provided through application configuration and a
+  map of optional overrides. If overrides are provided they will be merged with
+  the application configuration.
+  """
+  @spec new(map) :: t
+  def new(overrides \\ %{}) do
+    config =
+      Application.get_all_env(:shopify_graphql)
+      |> Enum.into(%{})
+      |> Map.merge(overrides)
+
+    struct(__MODULE__, config)
+  end
+end
