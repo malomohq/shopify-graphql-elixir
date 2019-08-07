09a008181ac96a72e58293bee04dd4f173422028
Anthony Smith <420061+anthonator@users.noreply.github.com>
2019-08-07 14:18:36 -0400

    Bump version to 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Shopify.GraphQL.MixProject do
   def project do
     [
       app: :shopify_graphql,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
