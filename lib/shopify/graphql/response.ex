f15820c0bdae6faef789ad3b18ae7a42c179ac7c
Anthony Smith <420061+anthonator@users.noreply.github.com>
2019-08-26 15:58:18 -0400

    Fix typespec for Response.new/2

--- lib/shopify/graphql/response.ex
+++ lib/shopify/graphql/response.ex
@@ -10,7 +10,7 @@ defmodule Shopify.GraphQL.Response do
 
   defstruct [:body, :headers, :status_code]
 
-  @spec new(map, Shopify.GraphQL.Config.t()) :: t
+  @spec new(Shopify.Client.response_t(), Shopify.GraphQL.Config.t()) :: t
   def new(response, config) do
     %__MODULE__{
       body: Helpers.JSON.decode(response.body, config),
