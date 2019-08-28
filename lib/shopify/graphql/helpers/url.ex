46cc6c40d8a5c6249ae0c1cbc7d9d6853cd51f91
Anthony Smith <420061+anthonator@users.noreply.github.com>
2019-08-27 20:25:49 -0400

    Modify Helpers.URL to provide URI and String representations of a URL

--- lib/shopify/graphql/helpers/url.ex
+++ lib/shopify/graphql/helpers/url.ex
@@ -1,12 +1,18 @@
 defmodule Shopify.GraphQL.Helpers.URL do
   @moduledoc false
 
-  @spec new(Shopify.GraphQL.Config.t()) :: String.t()
-  def new(config) do
+  @spec to_string(Shopify.GraphQL.Config.t()) :: String.t()
+  def to_string(config) do
+    config
+    |> to_uri()
+    |> URI.to_string()
+  end
+
+  @spec to_uri(Shopify.GraphQL.Config.t()) :: URI.t()
+  def to_uri(config) do
     %URI{ port: config.port, scheme: config.protocol }
     |> put_host(config)
     |> put_path(config)
-    |> URI.to_string()
   end
 
   defp put_host(uri, %{ shop: shop } = config) when not is_nil(shop) do
