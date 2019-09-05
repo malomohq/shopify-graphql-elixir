63b3abc7e09025bc784cb36d5aded71c913b3177
Anthony Smith <420061+anthonator@users.noreply.github.com>
2019-09-05 15:34:21 -0400

    Redid Limiter module

--- lib/shopify/graphql/limiter.ex
+++ lib/shopify/graphql/limiter.ex
@@ -1,35 +1,11 @@
 defmodule Shopify.GraphQL.Limiter do
-  @moduledoc """
-  Manage request throttling by the Shopify GraphQL API.
-
-  Shopify rate limits requests made to it's GraphQL API based on the complexity
-  of a query. If the query cost exceeds the available cost limit the API will
-  throttle the request and prevent the query from executing.
-
-  If you'd like to know more about Shopify rate limiting please see
-  [GraphQL Admin API rate limits](https://help.shopify.com/en/api/graphql-admin-api/graphql-admin-api-rate-limits).
-  """
-
   use DynamicSupervisor
 
-  alias Shopify.GraphQL.{ Helpers }
+  @type partition_id_t :: atom | String.t()
 
   #
   # client
   #
-
-  @doc """
-  Returns the identifier used to name `Shopify.GraphQL.Limiter.Shop` processes.
-
-  The identifier is the shop host name (e.g. `some-shop.myshopify.com`) and
-  can be extracted from a `Shopify.GraphQL.Config` struct.
-  """
-  @spec get_shop_id(Shopify.GraphQL.Config.t()) :: String.t()
-  def get_shop_id(config) do
-    config
-    |> Helpers.URL.to_uri()
-    |> Map.get(:host)
-  end
 
   @doc """
   Starts a `Shopify.GraphQL.Limiter` supervision tree and links it to the
@@ -37,17 +13,25 @@ defmodule Shopify.GraphQL.Limiter do
   """
   @spec start_link(Keyword.t()) :: Supervisor.on_start()
   def start_link(opts) do
-    DynamicSupervisor.start_link(__MODULE__, opts, name: get_name(opts))
+    DynamicSupervisor.start_link(__MODULE__, opts, name: Keyword.get(opts, :name, __MODULE__))
   end
 
   @doc """
-  Starts a `Shopify.GraphQL.Limiter.Shop` supervision tree.
+  Starts a partition.
   """
-  @spec start_shop(atom, String.t()) :: DynamicSupervisor.on_start_child()
-  def start_shop(server \\ __MODULE__, shop_id) do
-    spec = { __MODULE__.Shop, name: get_shop_name(server, shop_id) }
+  @spec start_partition(atom, partition_id_t) ::
+        { :ok, pid } | { :error, term }
+  def start_partition(server, partition_id) do
+    spec = { __MODULE__.Partition, parent: server, partition_id: partition_id }
 
-    DynamicSupervisor.start_child(server, spec)
+    case DynamicSupervisor.start_child(server, spec) do
+      { :ok, pid } ->
+        { :ok, pid }
+      { :error, { :already_started, pid } } ->
+        { :ok, pid }
+      otherwise ->
+        otherwise
+    end
   end
 
   #
@@ -57,17 +41,5 @@ defmodule Shopify.GraphQL.Limiter do
   @impl true
   def init(_opts) do
     DynamicSupervisor.init(strategy: :one_for_one)
-  end
-
-  #
-  # private
-  #
-
-  defp get_name(opts) do
-    Keyword.get(opts, :name, __MODULE__)
-  end
-
-  defp get_shop_name(server, shop_id) do
-    Module.concat([server, "Shop:#{shop_id}"])
   end
 end
--- test/shopify/graphql/limiter_test.exs
+++ test/shopify/graphql/limiter_test.exs
@@ -1,34 +1,41 @@
 defmodule Shopify.GraphQL.LimiterTest do
   use ExUnit.Case, async: true
 
-  alias Shopify.GraphQL.{ Config, Limiter }
-
-  test "get_shop_id/1" do
-    config = Config.new(%{ shop: "a-shop" })
-
-    assert Limiter.get_shop_id(config) == "a-shop.myshopify.com"
-  end
+  alias Shopify.GraphQL.{ Limiter }
 
   test "start_link/1", tags do
-    assert { :ok, _pid } = Limiter.start_link(name: process_name(tags))
-  end
-
-  test "start_shop/2", tags do
-    limiter = process_name(tags)
-
-    Limiter.start_link(name: limiter)
-
-    assert { :ok, _pid } = Limiter.start_shop(limiter, "a-shop.myshopify.com")
-  end
-
-  #
-  # private
-  #
-
-  defp process_name(tags) do
     line = Map.get(tags, :line)
     module = Map.get(tags, :module)
 
-    Module.concat(["#{module}:#{line}"])
+    name = Module.concat(["#{module}:#{line}"])
+
+    assert { :ok, _pid } = Limiter.start_link(name: name)
+  end
+
+  describe "start_partition/2" do
+    test "returns { :ok, pid } if not partition has been started", tags do
+      line = Map.get(tags, :line)
+      module = Map.get(tags, :module)
+
+      parent = Module.concat(["#{module}:#{line}"])
+      partition_id = line
+
+      Limiter.start_link(name: parent)
+
+      assert { :ok, _pid } = Limiter.start_partition(parent, partition_id)
+    end
+
+    test "returns { :ok, pid } if the partition has already been started", tags do
+      line = Map.get(tags, :line)
+      module = Map.get(tags, :module)
+
+      parent = Module.concat(["#{module}:#{line}"])
+      partition_id = line
+
+      Limiter.start_link(name: parent)
+
+      assert { :ok, _pid } = Limiter.start_partition(parent, partition_id)
+      assert { :ok, _pid } = Limiter.start_partition(parent, partition_id)
+    end
   end
 end
