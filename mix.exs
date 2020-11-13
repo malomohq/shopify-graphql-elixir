6ae3c0427c7263b468ef3882044ab12a1e970034
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-11-13 15:15:23 -0500

    Redo for v2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Shopify.GraphQL.MixProject do
   def project do
     [
       app: :shopify_graphql,
-      version: "1.6.0",
+      version: "2.0.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -23,28 +23,42 @@ defmodule Shopify.GraphQL.MixProject do
   defp deps do
     [
       { :gen_stage, ">= 0.14.0 and < 2.0.0", optional: true },
+
       { :hackney,   "~> 1.15", optional: true },
+
       { :jason,     "~> 1.1",  optional: true },
+
+      #
       # dev
-      { :dialyxir, "~> 1.0-rc", only: :dev, runtime: false },
+      #
+
+      { :dialyxir, "~> 1.0", only: :dev, runtime: false },
+
       { :ex_doc,   "> 0.0.0",   only: :dev, runtime: false },
-      # test
-      { :bypass, "~> 2.1.0-rc", only: :test }
     ]
   end
 
   defp package do
     %{
       description: "Elixir client for the Shopify GraphQL admin API",
+
       maintainers: ["Anthony Smith"],
+
       licenses: ["MIT"],
+
       links: %{
         GitHub: "https://github.com/malomohq/shopify-graphql-elixir",
+
         "Made by Malomo - Post-purchase experiences that customers love": "https://gomalomo.com"
       }
     }
   end
 
-  defp elixirc_paths(:test), do: ["lib", "test"]
-  defp elixirc_paths(_env), do: ["lib"]
+  defp elixirc_paths(:test) do
+    ["lib", "test"]
+  end
+
+  defp elixirc_paths(_env) do
+    ["lib"]
+  end
 end
